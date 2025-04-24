8aa17c784726fca3a0b367c6716c9d0240d2cf44
Jan Wohltmann <jan.wohltmann1@web.de>
2025-04-25 00:03:47 +0200

    added ability to enter a FEN-String as starting position

--- Guard_And_Towers/src/main/GamePanel.java
+++ Guard_And_Towers/src/main/GamePanel.java
@@ -6,6 +6,7 @@ import piece.Piece;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
@@ -17,6 +18,10 @@ public class GamePanel extends JPanel implements Runnable {
     Board board = new Board();
     Mouse mouse = new Mouse();
 
+    //Enter your FEN-String below to get a custom starting position (don't enter a string to get the normal starting position)
+    //String FEN;
+    String FEN = "r1r11RG1r1r1/2r11r12/3r13/7/3b13/2b11b12/b1b11BG1b1b1 r";
+
     //Pieces
     public static ArrayList<Piece> pieces = new ArrayList<>();
     public static ArrayList<Piece> simPieces = new ArrayList<>();
@@ -25,7 +30,7 @@ public class GamePanel extends JPanel implements Runnable {
     //Color
     public static final int BLUE = 0;
     public static final int RED = 1;
-    int currentColor = RED;
+    int currentColor;
 
     //Booleans
     boolean canMove;
@@ -48,25 +53,119 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void setPieces() {
-        //Blue Team
-        pieces.add(new Guard(BLUE,3,6, 1));
-        pieces.add(new Tower(BLUE,0,6, 1));
-        pieces.add(new Tower(BLUE,1,6, 1));
-        pieces.add(new Tower(BLUE,2,5, 1));
-        pieces.add(new Tower(BLUE,3,4, 1));
-        pieces.add(new Tower(BLUE,4,5, 1));
-        pieces.add(new Tower(BLUE,5,6, 1));
-        pieces.add(new Tower(BLUE,6,6, 1));
+        if (FEN == null) { //normal starting position
+            currentColor = RED;
 
-        //Red Team
-        pieces.add(new Guard(RED,3,0, 1));
-        pieces.add(new Tower(RED,0,0, 1));
-        pieces.add(new Tower(RED,1,0, 1));
-        pieces.add(new Tower(RED,2,1, 1));
-        pieces.add(new Tower(RED,3,2, 1));
-        pieces.add(new Tower(RED,4,1, 1));
-        pieces.add(new Tower(RED,5,0, 1));
-        pieces.add(new Tower(RED,6,0, 1));
+            //Blue Team
+            pieces.add(new Guard(BLUE,3,6, 1));
+            pieces.add(new Tower(BLUE,0,6, 1));
+            pieces.add(new Tower(BLUE,1,6, 1));
+            pieces.add(new Tower(BLUE,2,5, 1));
+            pieces.add(new Tower(BLUE,3,4, 1));
+            pieces.add(new Tower(BLUE,4,5, 1));
+            pieces.add(new Tower(BLUE,5,6, 1));
+            pieces.add(new Tower(BLUE,6,6, 1));
+
+            //Red Team
+            pieces.add(new Guard(RED,3,0, 1));
+            pieces.add(new Tower(RED,0,0, 1));
+            pieces.add(new Tower(RED,1,0, 1));
+            pieces.add(new Tower(RED,2,1, 1));
+            pieces.add(new Tower(RED,3,2, 1));
+            pieces.add(new Tower(RED,4,1, 1));
+            pieces.add(new Tower(RED,5,0, 1));
+            pieces.add(new Tower(RED,6,0, 1));
+        }
+        else { //custom starting position
+            //check if FEN-String is valid (probably doesn't filter out all incorrect FEN-Strings)
+            if (FEN.charAt(FEN.length() - 2) != ' ') {
+                throw new IllegalArgumentException("incorrect FEN-String: missing spacebar at the end");
+            }
+
+            if (FEN.charAt(FEN.length() - 1) == 'r') {
+                currentColor = RED;
+            }
+            else if (FEN.charAt(FEN.length() - 1) == 'b') {
+                currentColor = BLUE;
+            }
+            else {
+                throw new IllegalArgumentException("incorrect FEN-String: which player's turn is it?");
+            }
+
+            String newFEN = FEN.substring(0, FEN.length() - 2);
+            checkFENString(newFEN);
+
+            //turn FEN-String into correct position on the board
+            int col = 0;
+            int row = 0;
+            String tower = "no";
+            boolean guard = false;
+
+            try {
+                for (char c : newFEN.toCharArray()) {
+                    if (guard) {
+                        guard = false;
+                        col++;
+                    }
+                    else {
+                        if (c == 'r') {
+                            tower = "red";
+                        }
+                        else if (c == 'b') {
+                            tower = "blue";
+                        }
+                        else if (c == 'R') {
+                            guard = true;
+                            pieces.add(new Guard(RED,col,row, 1));
+                        }
+                        else if (c == 'B') {
+                            guard = true;
+                            pieces.add(new Guard(BLUE,col,row, 1));
+                        }
+                        else if (c == '/') {
+                            if (col != 7) {
+                                throw new IllegalArgumentException("incorrect FEN-String: wrong number of squares in row: " + (row + 1));
+                            }
+                            col = 0;
+                            row++;
+                        }
+                        else if (tower.equals("no")) {
+                            col += c - '0';
+                        }
+                        else if (tower.equals("red")){
+                            int number = c - '0';
+                            pieces.add(new Tower(RED,col,row,number));
+                            tower = "no";
+                            col++;
+                        }
+                        else if (tower.equals("blue")){
+                            int number = c - '0';
+                            pieces.add(new Tower(BLUE,col,row,number));
+                            tower = "no";
+                            col++;
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                throw new IllegalArgumentException("incorrect FEN-String");
+            }
+        }
+        //check for illegal amount of towers (more than 7)
+        int redTowers = 0;
+        int blueTowers = 0;
+        for (Piece p : pieces) {
+            if (p instanceof Tower) {
+                if (p.color == RED) {
+                    redTowers += p.Height;
+                }
+                else {
+                    blueTowers += p.Height;
+                }
+            }
+        }
+        if (redTowers > 7 || blueTowers > 7) {
+            throw new IllegalArgumentException("incorrect FEN-String: too many towers");
+        }
     }
 
     private void copyPieces(ArrayList<Piece> source, ArrayList<Piece> target) {
@@ -249,6 +348,39 @@ public class GamePanel extends JPanel implements Runnable {
             g2.setFont(new Font("Arial", Font.PLAIN, 50));
             g2.setColor(Color.white);
             g2.drawString(s, 730, 350);
+        }
+    }
+
+    private void checkFENString(String newFEN) {
+        if (!newFEN.matches("[rbRBG1234567/]+")) {
+            throw new IllegalArgumentException("incorrect FEN-String: wrong characters in String");
+        }
+
+        long count = newFEN.chars().filter(c -> c == '/').count();
+        if (count != 6) {
+            throw new IllegalArgumentException("incorrect FEN-String: incorrect number of '/'");
+        }
+
+        count = newFEN.chars().filter(c -> c == 'R').count();
+        if (count != 1) {
+            throw new IllegalArgumentException("incorrect FEN-String: incorrect number of 'R'");
+        }
+
+        count = newFEN.chars().filter(c -> c == 'B').count();
+        if (count != 1) {
+            throw new IllegalArgumentException("incorrect FEN-String: incorrect number of 'B'");
+        }
+
+        count = newFEN.chars().filter(c -> c == 'G').count();
+        if (count != 2) {
+            throw new IllegalArgumentException("incorrect FEN-String: incorrect number of 'G'");
+        }
+
+        if (!newFEN.contains("BG") || newFEN.indexOf("BG") != newFEN.lastIndexOf("BG")) {
+            throw new IllegalArgumentException("incorrect FEN-String: wrong amount of blue guards");
+        }
+        if (!newFEN.contains("RG") || newFEN.indexOf("RG") != newFEN.lastIndexOf("RG")) {
+            throw new IllegalArgumentException("incorrect FEN-String: wrong amount of red guards");
         }
     }
 }
